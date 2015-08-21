fd6608c591559a5f35374d339647a41bbe2f73fd
vcapicotto-ext@delfingen.com <vcapicotto-ext@delfingen.com>
2015-08-21 14:30:55 +0200

    modif

--- install.sh
+++ install.sh
@@ -170,8 +170,8 @@ function createBaseDonnees {
     RESULT=`mysqlshow --user=root --password=capic_20_04_1982 plowshare| grep -v Wildcard | grep -o plowshare`
     if [ "$RESULT" != "plowshare" ]; then
         echo "=== Création de la base de données ==="
-        mysql -uroot -pcapic_20_04_1982 -e "create database plowshare"
-        mysql -uroot -pcapic_20_04_1982 plowshare < $repertoire_git_plow_back/plowshare.sql
+        mysql -uroot -p -e "create database plowshare"
+        mysql -uroot -p plowshare < $repertoire_git_plow_back/plowshare.sql
     else
         echo "La base de données existe"
     fi
