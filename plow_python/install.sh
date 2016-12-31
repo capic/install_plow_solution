b5b2a6e49d881c5f8747918e51f2d448b9866b0a
capic <vcapic@gmail.com>
2016-12-31 12:18:45 +0100

    refactor

--- plow_python/install.sh
+++ plow_python/install.sh
@@ -24,7 +24,7 @@ function createConfigPythonFile {
     echo "PYTHON_LOG_CONSOLE_LEVEL=4" >> ${repertoire_git_plow_python}/config_python.cfg
     echo "PYHTON_LOG_FORMAT=\"[%(levelname)8s]  %(asctime)s <%(to_ihm)4s>     (%(file_name)s) {%(function_name)s} [%(message)s]\"" >> ${repertoire_git_plow_python}/config_python.cfg
     echo ""
-    echo "PYTHON_LOG_DIRECTORY=\"${repertoire_git_plow_python}/log\"" >> ${repertoire_git_plow_python}/config_python.cfg
+    echo "PYTHON_LOG_DIRECTORY=\"${repertoire_git_plow_python}log\"" >> ${repertoire_git_plow_python}/config_python.cfg
     echo "PYTHON_DIRECTORY_DOWNLOAD_TEMP=\"${repertoire_telechargement_temporaire}\"" >> ${repertoire_git_plow_python}/config_python.cfg
     echo "PYTHON_DIRECTORY_DOWNLOAD=\"${repertoire_telechargement}\"" >> ${repertoire_git_plow_python}/config_python.cfg
 }
@@ -61,7 +61,7 @@ function installPlowPython {
 
     echo "Adresse du dépot git de plow_pyhton : $git_plow_python => $repertoire_git_plow_python"
     echo "Téléchargement du gestionnaire de téléchargements"
-    git clone $git_plow_python $repertoire_git_plow_python
+    git clone -b dev $git_plow_python $repertoire_git_plow_python
     echo "Création du répertoire de log"
     mkdir ${repertoire_git_plow_python}/log
 
