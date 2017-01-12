88468cef26f71354fdf87070cea317458a3a669a
capic <capic_20_04_1982>
2017-01-12 16:25:59 +0100

    refactor plow notification

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ function installCommonPython {
         read install_pip
     fi
 
-    if ${install_pip} = 'o'; then
+    if [ ${install_pip} eq 'o' ]; then
         wget https://bootstrap.pypa.io/get-pip.py
         python get-pip.py
     fi
