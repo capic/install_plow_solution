3b3cec1408214106052955e1827feab9ac247beb
capic <vcapic@gmail.com>
2017-01-29 18:43:00 +0100

    start

---
+++ install_.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source $DIR/config/config_install.cfg
+
+if [[ $EUID -ne 0 ]]; then
+   echo "Ce script doit être lancé avec les droits super utilisateur" 1>&2
+   exit 1
+fi
+
+function start {
+    echo "=== Démarrage de l'installation ==="
+
+    echo "Installation de python 3"
+    apt install python3 pip3 -y
+
+    python3 ./main.py
+}
+
+start
