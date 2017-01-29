7d3b1f83fb7758efe77fb26d2b3ce0a8106983c4
capic <vcapic@gmail.com>
2017-01-29 19:14:41 +0100

    start

--- install_.sh
+++ install_.sh
@@ -11,7 +11,8 @@ function start {
     echo "=== Démarrage de l'installation ==="
 
     echo "Installation de python 3"
-    apt install python3 mysql-client mysql-connector-python python-dev -y
+    apt install python3 mysql-client python-dev -y
+    pip3 install mysql-connector-python
 
     python3 ./main.py
 }
