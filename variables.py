f52f5cd370c88972411a2e10a0e3971acaebf9ed
capic <capic_20_04_1982>
2017-01-30 11:04:49 +0100

    init

--- variables.py
+++ variables.py
@@ -35,7 +35,7 @@ def load_config():
     config = configparser.ConfigParser()
     config.read(os.path.dirname(os.path.abspath(__file__)) + "/config/config_install.cfg")
 
-    print(config.options())
+    print(config.options('DEFAULT'))
 
     repertoire_git_plow_python = config['repertoire_git_plow_python']
     repertoire_installation_base = config['repertoire_installation_base']
